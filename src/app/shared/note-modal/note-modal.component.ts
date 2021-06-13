12881798e4833de43199005b35c18766ab203f9d
Robin Singh <robinsinghkamboj@gmail.com>
2021-06-13 21:25:30 +0530

    -- reactive form implemented for add/edit note.

--- src/app/shared/note-modal/note-modal.component.ts
+++ src/app/shared/note-modal/note-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-note-modal',
@@ -7,8 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NoteModalComponent implements OnInit {
   @Input() featureSelected: string = "";
+  noteForm!: FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.noteForm = new FormGroup({
+      "title": new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      "content": new FormControl(null, [Validators.required, Validators.minLength(20)])
+    })
+  }
+
+  onSubmitNote() {
+    console.log(this.noteForm.value);
+  }
 }
